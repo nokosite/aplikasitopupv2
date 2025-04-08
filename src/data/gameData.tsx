54d41a7fa8f1bbeab58c33c82c845bcdbb069f85
MK <mahesyt@gmail.com>
2025-04-08 17:04:35 +0700

    Resolve merge conflict

--- src/data/gameData.tsx
+++ src/data/gameData.tsx
@@ -18,6 +18,14 @@ export const games = [
       products: [
         { id: '1', label: '70 Diamond', price: 10000 },
         { id: '2', label: '140 Diamond', price: 20000 },
+        { id: '3', label: '140 Diamond', price: 20000 },
+        { id: '4', label: '140 Diamond', price: 20000 },
+        { id: '5', label: '140 Diamond', price: 20000 },
+        { id: '6', label: '140 Diamond', price: 20000 },
+        { id: '7', label: '140 Diamond', price: 20000 },
+        { id: '8', label: '140 Diamond', price: 20000 },
+        { id: '9', label: '140 Diamond', price: 20000 },
+        { id: '10', label: '1400000 Diamond', price: 1999999 },
       ],
     },
     {
